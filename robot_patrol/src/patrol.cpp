08ca5f1b87c73f18fc28ab2eb8584de08d03724d
Pedro Araujo <araujopedrop@gmail.com>
2024-01-11 03:50:55 +0000

    CMakeList fixed - Fixing bugs in getting largest ray distance

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,9 +1,6 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "nav_msgs/msg/odometry.hpp"
-//#include "rclcpp/create_subscription.hpp"
 #include "rclcpp/rclcpp.hpp"
-//#include "rclcpp/subscription.hpp"
-#include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include <array>
@@ -21,20 +18,16 @@ class Patrol : public rclcpp::Node {
 public:
   Patrol() : Node("patrol_node_"), count_(0) {
 
+    // publisher to the /cmd_vel topic
     publisher_ =
         this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 
-    // subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-    //     "scan", 10, std::bind(&Patrol::topic_callback, this, _1));
+    subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/scan", 10, std::bind(&Patrol::topic_callback, this, _1));
 
-    // subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-    //     "/scan", 10, std::bind(&Patrol::topic_callback, this, _1));
-
-    /*
-
-    // timer_ = this->create_wall_timer(500ms,
-    // std::bind(&Topics_quiz_MoveRobot::timer_callback, this));
-    */
+    // control loop of 10 Hz
+    timer_ = this->create_wall_timer(
+        100ms, std::bind(&Patrol::publish_velocity, this));
   }
 
 private:
@@ -45,61 +38,51 @@ private:
       ranges[i] = msg->ranges[i];
     }
 
+    get_largest_distance_ray();
+
+    /*
+    RCLCPP_INFO(this->get_logger(), "ranges[360]: '%f'. Doing '%s' ",
+                msg->ranges[360], robot_action_.c_str());
+    */
+  }
+
+  // Control loop function
+  void publish_velocity() {
+
+    message.linear.x = 0.1;
+    message.angular.z = direction_ / 2;
+    publisher_->publish(message);
+  }
+
+  // Identify the largest distance ray (which is not an inf)
+  void get_largest_distance_ray() {
     for (int i = 0; i < 720; ++i) {
 
       // Left values
-      if (i > 0 && i < 20) {
-        if (ranges[i] < 1) {
-          robot_action_ = robot_action_right;
-          break;
-        }
-      }
-
-      // Front values
-      if (i > 20 && i < 700) {
-        if (ranges[i] > 1) {
-          robot_action_ = robot_action_forward;
-        } else {
-          robot_action_ = robot_action_left;
-          break;
-        }
-      }
-
-      if (i > 700 && i < 720) {
-        if (ranges[i] < 1) {
-          robot_action_ = robot_action_left;
-          break;
+      if (!(std::isinf(ranges[i]))) {
+        if (ranges[i] > direction_) {
+          // get angle
+          direction_ = -PI_ / 2 + ranges[i];
+          ray_value_ = i;
         }
       }
     }
 
-    if (robot_action_ == robot_action_forward) {
-      message.linear.x = 0.5;
-      message.angular.z = 0.0;
-    } else if (robot_action_ == robot_action_left) {
-      message.linear.x = 0.1;
-      message.angular.z = 0.75;
-    } else if (robot_action_ == robot_action_right) {
-      message.linear.x = 0.1;
-      message.angular.z = -0.75;
-    } else {
-      message.linear.x = 0.5;
-      message.angular.z = 0.0;
-    }
-
-    publisher_->publish(message);
-
-    RCLCPP_INFO(this->get_logger(), "ranges[360]: '%f'. Doing '%s' ",
-                msg->ranges[360], robot_action_.c_str());
+    RCLCPP_INFO(this->get_logger(), "Ray value: '%i'. Value '%f' ", ray_value_,
+                direction_);
   }
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
   float ranges[720];
-  std::float_t val_sensor = 0.0;
   geometry_msgs::msg::Twist message = geometry_msgs::msg::Twist();
   size_t count_;
+
+  int ray_value_ = 0;
+  float direction_ = -1.0;
+
+  double PI_ = M_PI;
   std::string robot_action_ = "";
   std::string robot_action_forward = "robot_action_forward";
   std::string robot_action_left = "robot_action_left";
